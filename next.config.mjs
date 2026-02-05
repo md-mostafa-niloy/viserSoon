2991f51b451b50bb11d4472172da6ee0ac417aa8
Md Mostafa Niloy <mostafa@visernic.com>
2026-02-05 20:41:54 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    optimizePackageImports: [
+      '@fortawesome/react-fontawesome', 
+      '@fortawesome/free-solid-svg-icons', 
+      'lucide-react',
+      'date-fns',
+      'clsx',
+      'tailwind-merge'
+    ],
+  },
 }
 
 export default nextConfig
